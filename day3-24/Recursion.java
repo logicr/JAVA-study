0143a661554f3d90dc83a4e97c9b4c4bc31b33e5
logicr <13314433445@163.com>
2018-03-26 22:19:34 +0800

    add note

--- day3-24/Recursion.java
+++ day3-24/Recursion.java
@@ -2,8 +2,8 @@ public class Recursion{
 	public static void main (String[] args){
 		System.out.println(recursion(60));
 	}
-	public static double recursion(double x){
-		if (1 < x){
+	public static double recursion(double x){//递归,注意这儿的double，int是装不下 60!的
+		if (1 < x){//递归出口
 			return recursion(x-1)*x;
 		}
 		else{
