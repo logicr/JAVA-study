66bcd1adfa6392204074d465e770fa31a35eb819
logicr <13314433445@163.com>
2018-03-28 08:55:35 +0800

    clean up

---
+++ day3-28/one/Test.java
@@ -0,0 +1,10 @@
+class Watch {
+	public int a;
+	 char b;
+}
+public class Test {
+	public static void main(String[] args) {
+		Watch wa = new Watch();
+		System.out.println("public int a ="+wa.a+ "\nchar b="+wa.b );
+	}
+}
